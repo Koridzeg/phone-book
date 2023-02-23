30fc69c5227e6266a8164fc271b50891f3d269e7
KoridzeG <giorgiqori@yahoo.com>
2023-02-24 01:59:09 +0400

    feat: phonebook controllers

---
+++ phone-book-backend/src/controllers/phonebook.controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import { Icontact } from "../interfaces/contact.model";
+import Contact from "../models/Contact";
+
+export const createContact = async (req: Request, res: Response) => {
+  const { name, surname, phone } = req.body;
+
+  try {
+    const contact = await Contact.create({ name, surname, phone });
+    res.status(201).json(contact);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to create contact" });
+  }
+};
+
+export const getAllContacts = async (req: Request, res: Response) => {
+  try {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to retrieve contacts" });
+  }
+};
+
+export const updateContact = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { name, surname, phone } = req.body;
+    const updateContact = await Contact.findByIdAndUpdate(
+      id,
+      { name, surname, phone },
+      { new: true }
+    );
+    res.status(200).json(updateContact);
+  } catch (error) {}
+};
+
+export const deleteContact = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const deleteContact = await Contact.findByIdAndDelete(id);
+    res.status(200).json(deleteContact);
+  } catch (err) {
+    res.status(500).json({ message: "error deleting contact", err });
+  }
+};
